7c110fdeb0dd81d71fbfae1dbd2bb243a7d0f88b
Mano <Mano@DESKTOP-FEB6J9S>
2023-02-21 18:56:32 +0200

    Add project files.

---
+++ PSI_Projektas_Komanda1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSI_Projektas_Komanda1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
