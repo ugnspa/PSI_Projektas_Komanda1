74ca8a4b9fadd9d6c326d5106275378537e685ef
Vytautas Jaras <vjaras202@gmail.com>
2023-02-21 19:08:59 +0200

    commiting

---
+++ PSI_Projektas_Komanda1/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Item
+{
+	public string Name { get; set; }
+	public string Type { get; set; }
+    public string Description { get; set; }
+
+
+    public Item()
+	{
+	}
+}
